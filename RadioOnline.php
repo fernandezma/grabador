b2c3d7aecd947355ac259688354d624a007658f7
Matias Fernandez <matias@fmlatribu.com>
2012-07-05 20:29:03 -0300

    usuarios radio online

--- RadioOnline.php
+++ RadioOnline.php
@@ -4,9 +4,6 @@ if(@file_get_contents('http://admin:lambare873@ns1.telepuerto.com.ar:8000/admin/
 	$escuchas = $xml->listeners;
 	$status = str_pad((int) $escuchas,4,"0",STR_PAD_LEFT);
 	echo "$status  ";
-
-//	echo "<div style=\"height:3px; width:100 ; background-color:#fff \"><div style=\"width:$status%; height: 3px; background-color:green\"> </div> </div>";
-		 
 }else{
 	echo 'Fuera de aire ';
 
