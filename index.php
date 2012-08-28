f8d99255077c84a6c378cb94763b7586f42d5430
fernandezma <matias@matiasba.com.ar>
2012-08-28 12:13:24 -0300

    acomodo codigo

--- index.php
+++ index.php
@@ -112,134 +112,119 @@ if ( $mode1 == 'generated' && $generated == 'pass' )
 ?>
 
 <div id="home">
-	<div id="header">
-                <div class="inactivo" id="titulo">
-		<h1><?php echo $titulo ?><h1>
-                </div>
-		<div id="tiempo">
-			<div id="corte">
-		                <span id="proximo">--:--</span></BR>
-				<span>PROXIMO CORTE </span>
-		        </div>
-			<div id="horaoficial">
-				<span>Hora Oficial</span></BR>
-				<span id="hsficial"> --:--:-- </span>
-			</div>
-			<div id="radioonline">
-				 <span>Escuchas</span></BR>
-				<span id="escuchas" >----</span>
-			</div>
-		</div>
-	</div>
-	<div id="anobar">
-		<div class="izquierda inactivo"><a href="<?php $anoiz= $anopro-1 ; echo "$base$anoiz/$mespro/$diapro" ;  ?>" > &lt; </a></div>
-		<div id="ano"><span id="fix5" ><?php echo $anopro ?> </span> </div>
-		<div class="derecha inactivo"><a href="<?php $anoiz= $anopro+1 ; echo "$base$anoiz/$mespro/$diapro" ;  ?>" > &gt; </a></div>
-	</div>
-	<div id="mesbar">
-                <ul>
-                <?php
-		$meses = array(ENE, FEB, MAR, ABR, MAY, JUN, JUL, AGO, SEP, OCT, NOV, DIC) ;
-                for ($i = 0; $i <= 11; $i++) {
-                        $urlmesmes = str_pad((int) $i+1,2,"0",STR_PAD_LEFT);
-                        $urlmes = "$base$anopro/$urlmesmes/$diapro";
+    <div id="header">
+	<div class="inactivo" id="titulo">
+	    <h1><?php echo $titulo ?><h1>
+        </div>
 
-			if ($mesac == $urlmesmes )
-                        {
-                                $mesclass= 'class="actual"';
-                        } elseif ($mespro == $urlmesmes) {
-				$mesclass= 'class="activo"';
-			} else {
-                                $mesclass= 'class=inactivo';
-                        }
+	<div id="tiempo">
 
-                        echo "<li $mesclass ><a href=$urlmes> $meses[$i] </a></li>";
-                }
-                ?>
-                </ul>
+	    <div id="corte">
+		<span id="proximo">--:--</span></BR>
+		<span>PROXIMO CORTE </span>
+	    </div>
+	
+	    <div id="horaoficial">
+		<span>Hora Oficial</span></BR>
+		<span id="hsficial"> --:--:-- </span>
+	    </div>
+
+	    <div id="radioonline">
+	        <span>Escuchas</span></BR>
+	        <span id="escuchas" >----</span>
+	    </div>
 
         </div>
-	<div id="dia">
-		<div id="fixdia">DIA:</div>
-		<ul>
-		<?php
-		for ($i = 1; $i <= 31; $i++) {
-			$urldiadia = str_pad((int) $i,2,"0",STR_PAD_LEFT);
-			$urldia = "$base$anopro/$mespro/$urldiadia";
-                        if ($diaac == $urldiadia )
-                        {
-                                $diaclass= 'class="actual"';
-                        } elseif ($diapro == $urldiadia) {
-                                $diaclass= 'class="activo"';
-                        } else {
-                                $diaclass= 'class=inactivo';
-                        }
 
-			echo "<li $diaclass ><a href=$urldia>$urldiadia</a></li>";
-		}
-		?>
-		</ul>
-			
-		
-	</div>
-	<div id="hora">
-                <?php
-		$diraudios= "/var/www/audios/$anopro-$mespro/$diapro";
+    </div>
 
-                for ($i = 0; $i <= 23; $i++) {
-                        $urlhorahora = str_pad((int) $i,2,"0",STR_PAD_LEFT);
-			$filename= "$anopro-$mespro-$diapro-$urlhorahora";
-			$urlhora = "$base$anopro/$mespro/$diapro/$urlhorahora";
-                        if ($horaac == $urlhorahora )
-                        {
-                                $horaclass= 'class="actual"';
-                        } elseif ($horapro == $urlhorahora) {
-                                $horaclass= 'class="activo"';
-                        } else {
-                                $horaclass= 'class=inactivo';
-                        }
-			echo "<li $horaclass ><a href=$urlhora>$urlhorahora Hs.</a><div id=exentos style=\"width:600px\"><ul>";
+    <div id="anobar">
+	<div class="izquierda inactivo"><a href="<?php $anoiz= $anopro-1 ; echo "$base$anoiz/$mespro/$diapro" ;  ?>" > &lt; </a></div>
+	<div id="ano"><span id="fix5" ><?php echo $anopro ?> </span> </div>
+	<div class="derecha inactivo"><a href="<?php $anoiz= $anopro+1 ; echo "$base$anoiz/$mespro/$diapro" ;  ?>" > &gt; </a></div>
+    </div>
 
-				$output = explode("\n", `/usr/bin/sudo /usr/local/bin/filestatus $diraudios $filename`); 
-				foreach($output as $line) 
-				{ 
-					if ($line == ''){
-					}else{
-					list($comienso, $archivo) =explode(',', $line);
-					if ($comienso > '01:00'){
-					echo "MAYOR";
-					$duracion = "60";
-					}else{
-						$duracion = `/usr/bin/sudo /usr/local/bin/fileinfo $diraudios/$archivo `;
-					}
-					list($hora, $minutos) =explode(':', $comienso);
-					$duracionfix= $duracion-($minutos+($hora*60));
-					echo "<li><div><div style=\"width:60;background-color:#919191;float:left;\"><div style=\"width:$minutos px;background-color:#919191;float:left;\">&nbsp;</div><div style=\"width:$duracionfix px;background-color:green;float:left;\">&nbsp;</div></div><div style=\"float:left;\"><a href=\"/audios/$anopro-$mespro/$diapro/$archivo\"> $archivo  </a> Inicio: $minutos grabado: $duracion </div></div> </li>";
-					}
-				}  
-//file = explode('/', $lsopen);	
-//				echo $file[1]."DUR:". $file[0];
-//				$timesss = shell_exec("/usr/bin/sudo /usr/local/bin/fileinfo $pathmp");
-//				list($hms, $milli) = explode('.', $timesss);
-//				list($hours, $minutes, $seconds) = explode(':', $hms);
-//				$total_seconds = "$minutes:$seconds";		
-//				echo "hoa";
-			//	echo "<li class=exentos >".$file."Duracion: ".$total_seconds.$iniciorec."</li>";
-				
-                        echo "</ul></div></li>";
-                }
-                ?>
+    <div id="mesbar">
+	<ul>
+        <?php
+        
+	$meses = array(ENE, FEB, MAR, ABR, MAY, JUN, JUL, AGO, SEP, OCT, NOV, DIC) ;
+        for ($i = 0; $i <= 11; $i++) {
+            $urlmesmes = str_pad((int) $i+1,2,"0",STR_PAD_LEFT);
+            $urlmes = "$base$anopro/$urlmesmes/$diapro";
 
+	    if ($mesac == $urlmesmes ){
+		$mesclass= 'class="actual"';
+	    } elseif ($mespro == $urlmesmes) {
+		$mesclass= 'class="activo"';
+	    } else {
+		$mesclass= 'class=inactivo';
+	    }
+	    echo "<li $mesclass ><a href=$urlmes> $meses[$i] </a></li>";
+        }
+        ?>
+        </ul>
+    </div>
 
-	</div>
-<div id="pie">
+    <div id="dia">
+        <div id="fixdia">DIA:</div>
+	<ul>
+	<?php
+	for ($i = 1; $i <= 31; $i++) {
+	    $urldiadia = str_pad((int) $i,2,"0",STR_PAD_LEFT);
+	    $urldia = "$base$anopro/$mespro/$urldiadia";
+	    if ($diaac == $urldiadia ){
+	        $diaclass= 'class="actual"';
+            } elseif ($diapro == $urldiadia) {
+	        $diaclass= 'class="activo"';
+            } else {
+	        $diaclass= 'class=inactivo';
+	    }
+	echo "<li $diaclass ><a href=$urldia>$urldiadia</a></li>";
+	}
+	?>
+	</ul>
+    </div>
+    <div id="hora">
+	<?php
+	$diraudios= "/var/www/audios/$anopro-$mespro/$diapro";
+        for ($i = 0; $i <= 23; $i++) {
+            $urlhorahora = str_pad((int) $i,2,"0",STR_PAD_LEFT);
+	    $filename= "$anopro-$mespro-$diapro-$urlhorahora";
+	    $urlhora = "$base$anopro/$mespro/$diapro/$urlhorahora";
+	    if ($horaac == $urlhorahora ) {
+                $horaclass= 'class="actual"';
+	    } elseif ($horapro == $urlhorahora) {
+	        $horaclass= 'class="activo"';
+	    } else {
+		$horaclass= 'class=inactivo';
+	    }
+	    echo "<li $horaclass ><a href=$urlhora>$urlhorahora Hs.</a><div id=exentos style=\"width:600px\"><ul>";
+	    $output = explode("\n", `/usr/bin/sudo /usr/local/bin/filestatus $diraudios $filename`); 
+	    foreach($output as $line) { 
+	        if ($line == ''){
+	        }else{
+	            list($comienso, $archivo) =explode(',', $line);
+		    if ($comienso > '01:00'){
+		        echo "MAYOR";
+	  	        $duracion = "60";
+		    }else{
+		        $duracion = `/usr/bin/sudo /usr/local/bin/fileinfo $diraudios/$archivo `;
+		    }
+		    list($hora, $minutos) =explode(':', $comienso);
+		    $duracionfix= $duracion-($minutos+($hora*60));
+		    echo "<li><div><div style=\"width:60;background-color:#919191;float:left;\"><div style=\"width:$minutos px;background-color:#919191;float:left;\">&nbsp;</div><div style=\"width:$duracionfix px;background-color:green;float:left;\">&nbsp;</div></div><div style=\"float:left;\"><a href=\"/audios/$anopro-$mespro/$diapro/$archivo\"> $archivo  </a> Inicio: $minutos grabado: $duracion </div></div> </li>";
+	        }
+	    }  
+            echo "</ul></div></li>";
+        }
+        ?>
+        </div>
+    <div id="pie">
 	<div id="ultimo">
 	</div>
+    </div>
 </div>
-
-</div>
-
-
 </BODY>
 </HTML>
 
